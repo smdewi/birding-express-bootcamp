3407c9ab817b3b9a6efc0aff5da21112b33d882d
smdewi <smdewi2020@gmail.com>
2022-03-09 01:40:27 +0800

    base

--- index.js
+++ index.js
@@ -1,1 +1,255 @@
-console.log('hello world');
+import express from 'express';
+import pg from 'pg';
+import jsSHA from 'jssha';
+import cookieParser from 'cookie-parser';
+
+// Initialise DB connection
+const { Pool } = pg;
+const pgConnectionConfigs = {
+  user: 'smdewi',
+  host: 'localhost',
+  database: 'lab_buddy',
+  port: 5432,
+};
+const pool = new Pool(pgConnectionConfigs);
+
+const app = express();
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+// Check user authentication
+// If user is a customer && is listed in the user table && the password matched then get the route
+app.post('/login', (request, response) => {
+  // retrieve the user entry using their email
+  const values = [request.body.username];
+  console.log(values);
+  pool.query('SELECT * from users WHERE username=$1', values, (error, result) => {
+    // return if there is a query error
+    if (error) {
+      console.log('Error executing query', error.stack);
+      response.status(503).send(result.rows);
+      return;
+    }
+
+    // we didnt find a user with that email
+    if (result.rows.length === 0) {
+      // the error for incorrect email and incorrect password are the same for security reasons.
+      // This is to prevent detection of whether a user has an account for a given service.
+      response.status(403).send('login failed!');
+      return;
+    }
+
+    // get user record from results
+    const user = result.rows[0];
+    console.log('User:', user);
+    // initialise SHA object
+    const shaObj = new jsSHA('SHA-512', 'TEXT', { encoding: 'UTF8' });
+    // input the password from the request to the SHA object
+    shaObj.update(request.body.password);
+    // get the hashed value as output from the SHA object
+    const hashedPassword = shaObj.getHash('HEX');
+
+    // eslint-disable-next-line max-len
+    // If the user's hashed password in the database does not match the hashed input password, login fails
+    if (user.password !== hashedPassword) {
+      // the error for incorrect email and incorrect password are the same for security reasons.
+      // This is to prevent detection of whether a user has an account for a given service.
+      response.status(403).send('login failed!');
+      return;
+    }
+
+    // The user's password hash matches that in the DB and we authenticate the user.
+    response.cookie('user_id', user.id);
+    // response.send('logged in!');
+    response.redirect('/user/new_order');
+  });
+});
+// Render Login page
+app.get('/login', (request, response) => {
+  response.render('login2');
+});
+
+app.get('/user/order_status', (request, response) => {
+  console.log('request came in');
+
+  const whenDoneWithQuery = (error, result) => {
+    if (error) {
+      console.log('Error executing query', error.stack);
+      response.status(503).send(result.rows);
+      return;
+    }
+    console.table(result.rows);
+    response.send(result.rows);
+  };
+
+  // Query using pg.Pool instead of pg.Client
+  pool.query('SELECT * from orders WHERE customer_id = 1', whenDoneWithQuery);
+});
+
+// Check lab user authentication and authorisation, then get the route
+app.get('/lab/order_status', (request, response) => {
+  console.log('request came in');
+
+  const whenDoneWithQuery = (error, result) => {
+    if (error) {
+      console.log('Error executing query', error.stack);
+      response.status(503).send(result.rows);
+      return;
+    }
+    console.table(result.rows);
+    // response.send(result.rows);
+    const order = result.rows;
+    console.log(order);
+    response.render('order-status', { order });
+  };
+
+  // Query using pg.Pool instead of pg.Client
+  pool.query('SELECT * FROM orders INNER JOIN customers ON customers.id = orders.customer_id', whenDoneWithQuery);
+});
+
+app.get('/lab/check_order', (request, response) => {
+  response.render('check-order');
+});
+
+app.post('/lab/check_order', (request, response) => {
+  // retrieve the user entry
+  const values = [request.body.order_number];
+  console.log(values);
+  pool.query('SELECT * from orders WHERE orders.id=$1', values, (error, result) => {
+    // return if there is a query error
+    if (error) {
+      console.log('Error executing query', error.stack);
+      response.status(503).send(result.rows);
+      return;
+    }
+
+    // if order number doesn't exist in orders table
+    if (result.rows.length === 0) {
+      response.status(403).send('No such order');
+      return;
+    }
+
+    // get user record from results
+    const order = result.rows[0];
+    console.log('Order:', order);
+    // response.send('Will display order here');
+    response.render('order-status.ejs', { order });
+  });
+});
+
+app.get('/user/new_order', (request, response) => {
+  response.render('order-form');
+});
+
+app.post('/user/new_order', (request, response) => {
+  // eslint-disable-next-line max-len
+  const valuesPatient = [request.body.patient_name, request.body.patient_nric, request.body.patient_dob, request.body.patient_address, request.body.patient_city, request.body.patient_country, request.body.patient_postal, request.body.patient_email, request.body.patient_phone];
+  console.log(valuesPatient);
+  pool.query('INSERT INTO patients (patient_name, patient_nric, patient_dob, patient_address, patient_city, patient_country, patient_postal, patient_email, patient_phone) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9) RETURNING id', valuesPatient, (error, result) => {
+    // return if there is a query error
+    if (error) {
+      console.log('Error executing query', error.stack);
+      response.status(503).send(result.rows);
+    }
+
+    // if query is successful
+    const patient = result.rows[0];
+    console.log('Patient ID: ', patient);
+    response.render('order-form2', { patient });
+  });
+});
+
+app.get('/user/new_order2', (request, response) => {
+  response.render('order-form2');
+});
+
+app.post('/user/new_order2', (request, response) => {
+  // eslint-disable-next-line max-len
+  const valuesNewOrder = [request.body.user_id, request.body.patient_id, request.body.order_date];
+  console.log(valuesNewOrder);
+  console.log(request.cookies.user_id);
+  pool.query('INSERT INTO orders (user_id, patient_id, order_date) VALUES ($1, $2, $3)', valuesNewOrder, (error, result) => {
+    // return if there is a query error
+    if (error) {
+      console.log('Error executing query', error.stack);
+      response.status(503).send(result.rows);
+    }
+
+    // if query is successful
+    const newOrder = result.rows[0];
+    console.log('New order: ', newOrder);
+    // response.send('Patients table updated. Orders table updated.');
+    response.render('order-form3');
+  });
+});
+
+app.get('/user/new_order3', (request, response) => {
+  response.render('order-form3');
+});
+
+app.post('/user/new_order3', (request, response) => {
+  // eslint-disable-next-line max-len
+  const valuesNewOrder = [request.body.order_id, request.body.test_id];
+  console.log(valuesNewOrder);
+  pool.query('INSERT INTO orders_tests (order_id, orderedtest_id) VALUES ($1, $2)', valuesNewOrder, (error, result) => {
+    // return if there is a query error
+    if (error) {
+      console.log('Error executing query', error.stack);
+      response.status(503).send(result.rows);
+    }
+
+    // if query is successful
+    const newOrder = result.rows[0];
+    console.log('New order: ', newOrder);
+    // eslint-disable-next-line max-len
+    // response.send('Patients table updated. Orders table updated. Orders_tests table updated. New order done');
+    const index = request.body.order_id;
+    response.redirect(`/user/order/${index}`);
+  });
+});
+
+// Get route to render a single order
+app.get('/user/order/:order_id', (request, response) => {
+  const index = request.params.order_id;
+  console.log('Index:', index);
+  pool.query(`SELECT order_id, test_id, name AS test_name, color AS tube_color FROM orders_tests INNER JOIN tests_tubes ON tests_tubes.test_id = orderedtest_id INNER JOIN tubes ON tubes.id = tests_tubes.tube_id INNER JOIN tests ON tests.id = tests_tubes.test_id WHERE order_id = ${index}`, (error, result) => {
+    if (error) {
+      console.log('Query Error');
+      response.status(503).send(result.rows);
+    }
+    const records = result.rows;
+    console.log('Order test data:', records);
+    response.render('order', { records });
+  });
+});
+
+// Get route to render all orders for a specific user
+app.get('/user/order_all', (request, response) => {
+  // Get userID from cookies
+  const userID = request.cookies.user_id;
+  console.log('UserID:', userID);
+  console.log('Cookies:', request.cookies.user_id);
+  pool.query(`SELECT username AS order_placed_by, patient_name, order_date, order_status FROM orders
+INNER JOIN users ON orders.user_id = users.id
+INNER JOIN patients ON orders.patient_id = patients.id
+WHERE orders.user_id = ${userID}`, (error, result) => {
+    if (error) {
+      console.log('Query Error');
+      response.status(503).send(result.rows);
+    }
+    const records = result.rows;
+    console.log('Order:', records);
+    // response.send('A List of all orders by one user');
+    response.render('order-list', { records });
+  });
+});
+
+app.get('/user/logout', (request, response) => {
+  response.clearCookie('user_id');
+  response.clearCookie('loggedIn');
+  response.redirect('/login');
+});
+
+app.listen(3004);
---
+++ init_tables.sql
@@ -0,0 +1,139 @@
+DROP TABLE IF EXISTS tubes CASCADE;
+DROP TABLE IF EXISTS tests CASCADE;
+DROP TABLE IF EXISTS tests_tubes CASCADE;
+DROP TABLE IF EXISTS customers CASCADE;
+DROP TABLE IF EXISTS patients CASCADE;
+DROP TABLE IF EXISTS orders CASCADE;
+DROP TABLE IF EXISTS orders_tests CASCADE;
+DROP TABLE IF EXISTS users;
+
+CREATE TABLE tubes (
+  id SERIAL PRIMARY KEY,
+  color TEXT,
+  info TEXT,
+  order_of_draw INTEGER
+);
+
+CREATE TABLE tests (
+  id SERIAL PRIMARY KEY,
+  name TEXT,
+  info TEXT
+);
+
+CREATE TABLE customers (
+  id SERIAL PRIMARY KEY,
+  name TEXT,
+  street TEXT,
+  city TEXT,
+  postal INTEGER,
+  country TEXT,
+  email TEXT,
+  phone INTEGER
+);
+
+CREATE TABLE patients (
+  id SERIAL PRIMARY KEY,
+  patient_name TEXT,
+  patient_nric TEXT,
+  patient_dob TEXT,
+  patient_address TEXT,
+  patient_city TEXT,
+  patient_country TEXT,
+  patient_postal TEXT,
+  patient_email TEXT,
+  patient_phone INTEGER
+);
+
+CREATE TABLE tests_tubes (
+  id SERIAL PRIMARY KEY,
+  test_id SERIAL,
+  tube_id SERIAL,
+  CONSTRAINT fk_test FOREIGN KEY(test_id) REFERENCES tests(id),
+  CONSTRAINT fk_tube FOREIGN KEY(tube_id) REFERENCES tubes(id)
+);
+
+CREATE TABLE users (
+  id SERIAL PRIMARY KEY,
+  customer_id SERIAL,
+  username TEXT UNIQUE,
+  password TEXT,
+  first_name TEXT,
+  last_name TEXT,
+  email TEXT,
+  CONSTRAINT fk_customer FOREIGN KEY(customer_id) REFERENCES customers(id)
+);
+
+CREATE TABLE orders (
+  id SERIAL PRIMARY KEY,
+  user_id SERIAL,
+  patient_id SERIAL,
+  order_date TEXT,
+  order_status TEXT,
+  CONSTRAINT fk_customer FOREIGN KEY(user_id) REFERENCES users(id),
+  CONSTRAINT fk_patient FOREIGN KEY(patient_id) REFERENCES patients(id)
+);
+
+CREATE TABLE orders_tests (
+  id SERIAL PRIMARY KEY,
+  order_id SERIAL,
+  orderedtest_id SERIAL,
+  isAccepted BOOLEAN,
+  comment TEXT,
+  CONSTRAINT fk_order FOREIGN KEY(order_id) REFERENCES orders(id),
+  CONSTRAINT fk_test FOREIGN KEY(orderedtest_id) REFERENCES tests(id)
+);
+
+
+
+INSERT INTO tubes (color, info, order_of_draw) VALUES ('light blue', 'Sodium Citrate', 1);
+INSERT INTO tubes (color, info, order_of_draw) VALUES ('red', 'Plain', 2);
+INSERT INTO tubes (color, info, order_of_draw) VALUES ('gold', 'GEL', 3);
+INSERT INTO tubes (color, info, order_of_draw) VALUES ('royal blue', 'Trace Element', 4);
+INSERT INTO tubes (color, info, order_of_draw) VALUES ('green', 'Heparin', 5);
+INSERT INTO tubes (color, info, order_of_draw) VALUES ('lavender', 'EDTA', 6);
+INSERT INTO tubes (color, info, order_of_draw) VALUES ('grey', 'Sodium Fluoride', 7);
+
+INSERT INTO tests (name, info) VALUES ('HBA', 'Glycated Haemoglobin (HbA1c)');
+INSERT INTO tests (name, info) VALUES ('GLU', 'Glucose (fasting)');
+INSERT INTO tests (name, info) VALUES ('HY01', 'Hypertension profile');
+INSERT INTO tests (name, info) VALUES ('DFS1', 'Dengue fever profile');
+
+INSERT INTO customers (name, street, city, postal, country, email, phone) VALUES ('Sunshine Family Clinic', 'Blk 491 Jurong West', 'Singapore', 648161, 'Singapore', 'sunshine_clinic@gmail.com', 63982258);
+INSERT INTO customers (name, street, city, postal, country, email, phone) VALUES ('Mount Sinai Hospital', '3 Mount Sinai Road', 'Singapore', 228510, 'Singapore', 'mtsinai@gmail.com', 67312218);
+INSERT INTO customers (name, street, city, postal, country, email, phone) VALUES ('National Dialysis Centre', '825 Woodlands', 'Singapore', 730825, 'Singapore', 'ndc@gmail.com', 63651810);
+
+INSERT INTO patients (patient_name, patient_nric, patient_dob, patient_address, patient_city, patient_country, patient_postal, patient_email, patient_phone) VALUES ('Tan Tjie Kiong', 'B19492010', '20/01/1949', 'Ploso Baru 75', 'Surabaya', 'Indonesia', 206254, 'ttk@gmail.com', 7341628);
+INSERT INTO patients (patient_name, patient_nric, patient_dob, patient_address, patient_city, patient_country, patient_postal, patient_email, patient_phone) VALUES ('Denyse Lim', 'S444444', '20/05/1940', 'Jalan Seroja 28', 'Johor Bahru', 'Malaysia', 814000, 'dlim@gmail.com', 60235689);
+INSERT INTO patients (patient_name, patient_nric, patient_dob, patient_address, patient_city, patient_country, patient_postal, patient_email, patient_phone) VALUES ('John Barry', 'X55544', '31/10/1941', '44 Main St', 'Providence', 'USA', 211444, 'jbarry@conspiracy.com', 44444444);
+INSERT INTO patients (patient_name, patient_nric, patient_dob, patient_address, patient_city, patient_country, patient_postal, patient_email, patient_phone) VALUES ('Mary Koh', 'P777777', '07/07/1987', 'Blk 480 Jurong West 41', 'Singapore', 'Singapore', 648162, 'mkoh@gmail.com', 15973684);
+INSERT INTO patients (patient_name, patient_nric, patient_dob, patient_address, patient_city, patient_country, patient_postal, patient_email, patient_phone) VALUES ('Roshni Patel', 'A123456', '14/02/1996', '47 Gardner Street', 'Boston', 'USA', 617845, 'rpatel@gmail.com', 34256689);
+INSERT INTO patients (patient_name, patient_nric, patient_dob, patient_address, patient_city, patient_country, patient_postal, patient_email, patient_phone) VALUES ('Ismail Hasan', 'B232323', '07/06/2000', '40 Bukit Batok St 21', 'Singapore', 'Singapore', 588187, 'ismail@gmail.com', 78453625);
+
+INSERT INTO tests_tubes (test_id, tube_id) VALUES (1, 6);
+INSERT INTO tests_tubes (test_id, tube_id) VALUES (2, 7);
+INSERT INTO tests_tubes (test_id, tube_id) VALUES (3, 2);
+INSERT INTO tests_tubes (test_id, tube_id) VALUES (3, 6);
+INSERT INTO tests_tubes (test_id, tube_id) VALUES (3, 7);
+INSERT INTO tests_tubes (test_id, tube_id) VALUES (4, 2);
+INSERT INTO tests_tubes (test_id, tube_id) VALUES (4, 6);
+
+INSERT INTO users (customer_id, username, password, first_name, last_name, email) VALUES (1, 'betty','79a21f17d71da2c0e851479b36ab9db174df676ad36d479b3ba524b6343b3790f8295b0367d8f0e71f1254c1ab34b9f49e4d27197371a2cd6f531a4b1716b18a', 'Betty', 'Wong', 'bwong@sunshineclinic.com');
+INSERT INTO users (customer_id, username, password, first_name, last_name, email) VALUES (2,'adam', '574e5555ff92d0acda78b4c8e89350486e7a0d6b943051a51f8a18651884bbf987bbd461e2f5a2e7f640f927dca9daf942cc29d0f9285d94265e711afdddd2d9', 'Adam', 'Smith', 'asmith@mtsinai.com');
+INSERT INTO users (customer_id, username, password, first_name, last_name, email) VALUES (3,'ali','15235cf272b760adab86e7bd543d8da89898166c58ba40dd922cab4151a0919b01f6e51d22e06e3c182c2132115800158877539b2c06af21041caae86be1c354', 'Ali', 'Wibowo', 'ali@ndc.com');
+
+INSERT INTO orders (user_id, patient_id, order_date, order_status) VALUES (1, 1, '1/2/2022', 'ordered');
+INSERT INTO orders (user_id, patient_id, order_date, order_status) VALUES (1, 2, '1/2/2022', 'ordered');
+INSERT INTO orders (user_id, patient_id, order_date, order_status) VALUES (2, 3, '2/2/2022', 'ordered');
+INSERT INTO orders (user_id, patient_id, order_date, order_status) VALUES (2, 4, '2/2/2022', 'ordered');
+INSERT INTO orders (user_id, patient_id, order_date, order_status) VALUES (3, 5, '3/2/2022', 'ordered');
+INSERT INTO orders (user_id, patient_id, order_date, order_status) VALUES (3, 6, '3/2/2022', 'ordered');
+
+INSERT INTO orders_tests (order_id, orderedtest_id) VALUES (1, 1);
+INSERT INTO orders_tests (order_id, orderedtest_id) VALUES (1, 2);
+INSERT INTO orders_tests (order_id, orderedtest_id) VALUES (2, 3);
+INSERT INTO orders_tests (order_id, orderedtest_id) VALUES (3, 4);
+INSERT INTO orders_tests (order_id, orderedtest_id) VALUES (4, 1);
+INSERT INTO orders_tests (order_id, orderedtest_id) VALUES (4, 2);
+INSERT INTO orders_tests (order_id, orderedtest_id) VALUES (5, 3);
+INSERT INTO orders_tests (order_id, orderedtest_id) VALUES (6, 4);
+
